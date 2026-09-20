60e3a485d8697660e8691a201cc7b5caf995704d
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-20 09:58:24 +0530

    Add Easy Java Solution for 69. Sqrt(x) from LeetCode

---
+++ LeetCode/Java/Easy/69. Sqrtx/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        int sqrt = 0;
+        while(true){
+        
+            if(sqrt*sqrt < x){
+                sqrt++;
+                if(sqrt*sqrt > x){
+                    return sqrt -1 ;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        return sqrt;
+    }
+}
