ec39f770f5452563afa84a6b1b8c6ba5b0bfe871
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-21 00:12:26 +0530

    Add Easy Java Solution for 69. Sqrt(x) from LeetCode

--- LeetCode/Java/Easy/69. Sqrtx/README.md
+++ LeetCode/Java/Easy/69. Sqrtx/README.md
@@ -8,8 +8,8 @@
 Math, Binary Search, Newton's Method
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 35 ms
+- **Memory:** 42.7 MB
 
 ---
 
--- LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
+++ LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
@@ -1,24 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
 
-        if( x < 2){
-            return x;
-        }
-
-        int left = 0;
-        int right = x/2;
-        int answer = 0;
-
-        while(left <= right){
-            int mid = left + (right - left)/2;
-
-            if(mid <= x/mid){
-                answer = mid;
-                left = mid + 1;
-            }else{
-                right = mid - 1;
+       long sqrt = 0;
+        while(true){
+        
+            if(sqrt*sqrt < x){
+                sqrt++;
+                if(sqrt*sqrt > x){
+                    return (int) sqrt - 1 ;
+                }
+            }
+            else{
+                break;
             }
         }
-        return answer;
+        return (int) sqrt;
     }
 }
