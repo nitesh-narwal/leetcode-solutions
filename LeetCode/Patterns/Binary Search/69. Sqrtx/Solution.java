0dd769d37eedd2b2978382e7233f3157d70b2632
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-20 10:03:46 +0530

    Add Easy Java Solution for 69. Sqrt(x) from LeetCode

--- LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
+++ LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
 
-       int sqrt = 0;
+       long sqrt = 0;
         while(true){
         
             if(sqrt*sqrt < x){
                 sqrt++;
                 if(sqrt*sqrt > x){
-                    return sqrt - 1 ;
+                    return (int) sqrt - 1 ;
                 }
             }
             else{
                 break;
             }
         }
-        return sqrt;
+        return (int) sqrt;
     }
 }
