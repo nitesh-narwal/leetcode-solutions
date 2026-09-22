b131a36f1b3183058b90e26e97866dc201549f51
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-22 23:09:07 +0530

    Add Easy Java Solution for 367. Valid Perfect Square from LeetCode

---
+++ LeetCode/Java/Easy/367. Valid Perfect Square/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 1;
+        int right = num/2;
+        boolean var = false;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if (mid == num / mid && num % mid == 0) {
+                return true;
+            }
+            else if (mid > num / mid) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return var;
+    }
+}
