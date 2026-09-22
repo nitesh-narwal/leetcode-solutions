5228de64a864c0114e10f51df5e95a6997797518
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-23 00:07:36 +0530

    Add Easy Java Solution for 441. Arranging Coins from LeetCode

---
+++ LeetCode/Patterns/Binary Search/441. Arranging Coins/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int left = 1; 
+        int right = n;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(2*n < mid*(mid + 1)){
+                return mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return 0;
+    }
+}
