a62d176df20ca248b8e3f4db7bd279ecae906619
Nitesh Narwal <nnehrwal2002@gmail.com>
2026-09-25 16:26:01 +0530

    Add Easy Java Solution for 1539. Kth Missing Positive Number from LeetCode

---
+++ LeetCode/Patterns/Binary Search/1539. Kth Missing Positive Number/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+
+        ArrayList<Integer> demo = new ArrayList<>();
+
+        int left = 0;
+        int right = arr.length - 1;
+        int val = 1;
+        int end = arr.length + 2*k;
+
+        while( val != -1){
+            boolean rtn = BS(arr, left, right, val);
+
+            if(!rtn){
+                demo.add(val);
+            }
+
+            if( val == end ){
+                val = -1;
+                break;
+            }
+            val++;
+        }
+
+        return demo.get(k + 2);
+
+    }
+
+     static boolean BS(int[] arr, int left, int right, int target){
+        int mid = left + (right - left)/2;
+
+        if(arr[mid] < target){
+            left = mid + 1;
+        }
+        else if(arr[mid] > target){
+            right = mid - 1;
+        }
+        else{
+            return true;
+        }
+
+        return false;
+     }
+
+}
